ca0e3b2d105a979dfe6bc1658bbb03bfd77d3f9b
Jordan139 <jordan139@hotmail.co.uk>
2016-10-14 10:07:56 +0100

    basic addition

--- ECS_System/src/ecs_system/helloWorld.java
+++ ECS_System/src/ecs_system/helloWorld.java
@@ -14,6 +14,12 @@ public class helloWorld {
     
      public static void main(String[] args) {
         System.out.print("Hello World!!");
+        int a;
+        int b;
+        a = 100;
+        b = 200;
+                
+        System. out.print(a + b);
     }
     
     
