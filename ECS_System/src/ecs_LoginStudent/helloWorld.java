e90398048f4489f665aa52c74b816768954afddb
pheon <pheon@10.1.253.190>
2016-11-06 18:47:22 +0000

    testing

---
+++ ECS_System/src/ecs_LoginStudent/helloWorld.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ecs_LoginStudent;
+
+/**
+ *
+ * @author pheon
+ */
+public class helloWorld 
+        
+        System.out.println("Hellow world");
+    
+}
