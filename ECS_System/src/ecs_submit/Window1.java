7efabef1dbe0d1819fe143831d16a29196f630f9
Jeamathu <Jeamathu@JT>
2016-11-28 11:38:22 +0000

    Gui improvement

--- ECS_System/src/ecs_submit/Window1.java
+++ ECS_System/src/ecs_submit/Window1.java
@@ -104,7 +104,7 @@ public class Window1 extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
-        JFrame Window3 = new Window2();
+        JFrame Window3 = new Window21();
     Window3.setVisible(true);
     }//GEN-LAST:event_jButton1ActionPerformed
 
