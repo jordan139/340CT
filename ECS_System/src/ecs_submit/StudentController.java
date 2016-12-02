7122c9de669d11262d2c708461a47479f3f16df6
Jeamathu <Jeamathu@gbbcstmpl01.coventry.ac.uk>
2016-12-02 09:39:41 +0000

    Commit successfully

--- ECS_System/src/ecs_submit/StudentController.java
+++ ECS_System/src/ecs_submit/StudentController.java
@@ -9,6 +9,43 @@ package ecs_submit;
  *
  * @author baheeraj
  */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import Utilities.FileUtils;
+
 public class StudentController {
     
+    String connectionURL = "jdbc:derby://localhost:1527/COURSEWORK";
+    String uName = "jey";
+    String uPass = "123";
+    
+    public ArrayList getAll() {
+        ArrayList<String> courseworks = new ArrayList<>();
+        String temp;
+        //ConnectionURL, username and password should be specified in getConnection()       
+        try {
+            Connection conn = DriverManager.getConnection(connectionURL, uName, uPass);
+            System.out.println("Connecting to database..."); //inform user trying to connect to the database
+            //if a secure connection has been made 
+
+            if (conn != null) {
+                Statement st = conn.createStatement();
+                ResultSet rs = null;
+
+                String sql = "SELECT * FROM COURSEWORK";
+                rs = st.executeQuery(sql);
+                while (rs.next()) {
+                    temp = rs.getInt("COURSE_TITLE") + "\t" + rs.getString("MODULE_TITLE")+ rs.getString("COURSE_CREATION") + "\n";
+                    courseworks.add(temp);
+                }
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+        return courseworks;
+    }
 }
